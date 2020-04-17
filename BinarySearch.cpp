2a15478e41e751f192890a028c988cb6f1c8ec53
SAY-droid427 <sayanimallick0218@gmail.com>
2020-04-17 22:06:31 +0530

    Adding binary search

---
+++ BinarySearch.cpp
@@ -0,0 +1,41 @@
+//Binary search
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int a[],int l,int r,int key)
+{
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+  
+        // If the element is present at the middle 
+        // itself 
+        if (arr[mid] == x) 
+            return mid; 
+  
+        // If element is smaller than mid, then 
+        // it can only be present in left subarray 
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid - 1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch(arr, mid + 1, r, x); 
+    } 
+  
+    // We reach here when element is not 
+    // present in array 
+    return -1; 
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    int key;
+    cin>>key;
+    cout<<binarySearch(a,0,n-1,key);
+    return 0;
+}
+
