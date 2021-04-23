4d1bd8af2a90c7a76107ea7aee96bbb2c5b2c6c0
Rishabh Dwivedi <57454462+Rishabh062@users.noreply.github.com>
2021-04-23 21:29:55 +0530

    Update shell_sort.cpp
    Added multiple comments which has made code easier to understand.
    Added:
    1) About problem
    2) Time and Space complexity
    3) Some algorithm about shell sort

--- Implementations/C++/Sorting Algorithms/shell_sort.cpp
+++ Implementations/C++/Sorting Algorithms/shell_sort.cpp
@@ -1,7 +1,21 @@
+// Shell sort is often termed as an improvement over insertion sort.In shell sort, the list is sorted by breaking it down into a number of smaller sublists,
+// shell sort technique uses increment i, which is also called “gap” and uses it to create a list of elements that are “i” elements apart.
+
+// Complexity for shell sort.
+/*
+1) Timecomplexity
+    best case time complexity is O(n* logn)
+    worst case is O(n* log2n).
+    Time complexity of Shell sort is generally assumed to be near to O(n) and less than O(n^2).
+    
+2)Spacecomplexity
+   Θ(1)
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
 void display(int *array,int size){
+    // This function is use for displaying values.
     for(int i=0;i<size;i++){
         cout<<array[i]<<" ";
     }
@@ -9,6 +23,7 @@ void display(int *array,int size){
 }
 
 void swapping(int &a, int &b){
+    // This function is use for swapping the values.
     int temp;
     temp=a;
     a=b;
@@ -16,6 +31,7 @@ void swapping(int &a, int &b){
 }
 
 void shellSort(int *arr,int n){
+    // This function is use for sorting. Therefore it creates a list of element.
     int gap,j,k;
     for(gap=n/2;gap>0;gap=gap/2){
         for(j=gap;j<n;j++){
@@ -32,6 +48,7 @@ void shellSort(int *arr,int n){
 }
 
 int main(){
+    // Driver code which is use to take value from user and pass it to main function.
     int n;
     cout<<"Enter the number of elements: ";
     cin>>n;
@@ -46,5 +63,5 @@ int main(){
     cout<<"Array after Sorting: ";
     display(arr,n);
 
-  
 }
+// Shell sort is the highly efficient algorithm that comes to an improvement over the insertion sort.
